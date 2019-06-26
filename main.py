d294432e3852761c111c52c945f6e01dc0d5d699
Pedro Verruma <pedro.verruma@talkdesk.com>
2019-06-26 21:07:16 +0100

    Code cleanup.

--- main.py
+++ main.py
@@ -14,41 +14,24 @@ from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Concatenate, 
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import EarlyStopping
 import warnings
+
+#region Setup
+
 pd.set_option("display.max_colwidth", 200)
 warnings.filterwarnings("ignore")
+
 nltk.download('stopwords')
+contraction_mapping = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not","didn't": "did not", "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not","he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is","I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would","i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would","it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam","mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have","mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock","oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have","she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is","should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as","this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would","there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have","they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have","wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are","we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are","what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is","where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have","why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have","would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all","y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have","you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have","you're": "you are", "you've": "you have"}
+stop_words = set(stopwords.words('english'))
 
-print("Starting...")
+#endregion
 
-data=pd.read_csv("Reviews.csv",nrows=100000)
-data.drop_duplicates(subset=['Text'],inplace=True)  #dropping duplicates
-data.dropna(axis=0,inplace=True)   #dropping na
+#region Data Load and Clean
 
-contraction_mapping = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not",
-                           "didn't": "did not", "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not",
-                           "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",
-                           "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would",
-                           "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would",
-                           "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam",
-                           "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have",
-                           "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock",
-                           "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have",
-                           "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is",
-                           "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as",
-                           "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would",
-                           "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have",
-                           "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have",
-                           "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are",
-                           "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",
-                           "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is",
-                           "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have",
-                           "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have",
-                           "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all",
-                           "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have",
-                           "you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have",
-                           "you're": "you are", "you've": "you have"}
+data = pd.read_csv("Reviews.csv",nrows=100000)
+data.drop_duplicates(subset=['Text'],inplace=True) # Dropping duplicates
+data.dropna(axis=0,inplace=True) # Dropping na
 
-stop_words = set(stopwords.words('english')) 
 def text_cleaner(text,num):
     newString = text.lower()
     newString = BeautifulSoup(newString, "lxml").text
@@ -76,17 +59,17 @@ cleaned_summary = []
 for t in data['Summary']:
     cleaned_summary.append(text_cleaner(t,1))
 
-data['cleaned_text']=cleaned_text
-data['cleaned_summary']=cleaned_summary
+data['cleaned_text'] = cleaned_text
+data['cleaned_summary'] = cleaned_summary
 
 data.replace('', np.nan, inplace=True)
 data.dropna(axis=0,inplace=True)
 
-max_text_len=30
-max_summary_len=8
+max_text_len = 30
+max_summary_len = 8
 
-cleaned_text =np.array(data['cleaned_text'])
-cleaned_summary=np.array(data['cleaned_summary'])
+cleaned_text = np.array(data['cleaned_text'])
+cleaned_summary= np.array(data['cleaned_summary'])
 
 short_text=[]
 short_summary=[]
@@ -96,19 +79,23 @@ for i in range(len(cleaned_text)):
         short_text.append(cleaned_text[i])
         short_summary.append(cleaned_summary[i])
         
-df=pd.DataFrame({'text':short_text,'summary':short_summary})
+df = pd.DataFrame({'text':short_text,'summary':short_summary})
 
-df['summary'] = df['summary'].apply(lambda x : 'sostok '+ x + ' eostok')
+df['summary'] = df['summary'].apply(lambda x : 'sostok ' + x + ' eostok')
 
-x_tr,x_val,y_tr,y_val=train_test_split(np.array(df['text']),np.array(df['summary']),test_size=0.1,random_state=0,shuffle=True)
+#endregion
 
-# --
+#region Dataset spliting
 
+x_tr, x_val, y_tr, y_val = train_test_split(np.array(df['text']), np.array(df['summary']), test_size=0.1, random_state=0, shuffle=True)
+
+#endregion
+
+#region Tokenizer for reviews
 x_tokenizer = Tokenizer() 
 x_tokenizer.fit_on_texts(list(x_tr))
 
 thresh=4
-
 cnt=0
 tot_cnt=0
 freq=0
@@ -124,27 +111,27 @@ for key,value in x_tokenizer.word_counts.items():
 print("% of rare words in vocabulary:",(cnt/tot_cnt)*100)
 print("Total Coverage of rare words:",(freq/tot_freq)*100)
 
-#prepare a tokenizer for reviews on training data
 x_tokenizer = Tokenizer(num_words=tot_cnt-cnt) 
 x_tokenizer.fit_on_texts(list(x_tr))
 
 #convert text sequences into integer sequences
-x_tr_seq    =   x_tokenizer.texts_to_sequences(x_tr) 
-x_val_seq   =   x_tokenizer.texts_to_sequences(x_val)
+x_tr_seq = x_tokenizer.texts_to_sequences(x_tr) 
+x_val_seq = x_tokenizer.texts_to_sequences(x_val)
 
 #padding zero upto maximum length
-x_tr    =   pad_sequences(x_tr_seq,  maxlen=max_text_len, padding='post')
-x_val   =   pad_sequences(x_val_seq, maxlen=max_text_len, padding='post')
+x_tr = pad_sequences(x_tr_seq,  maxlen=max_text_len, padding='post')
+x_val = pad_sequences(x_val_seq, maxlen=max_text_len, padding='post')
 
 #size of vocabulary ( +1 for padding token)
-x_voc   =  x_tokenizer.num_words + 1
+x_voc = x_tokenizer.num_words + 1
 
-#prepare a tokenizer for reviews on training data
+#endregion
+
+#region Tokenizer for summaries
 y_tokenizer = Tokenizer()   
 y_tokenizer.fit_on_texts(list(y_tr))
 
 thresh=6
-
 cnt=0
 tot_cnt=0
 freq=0
@@ -160,27 +147,23 @@ for key,value in y_tokenizer.word_counts.items():
 print("% of rare words in vocabulary:",(cnt/tot_cnt)*100)
 print("Total Coverage of rare words:",(freq/tot_freq)*100)
 
-#prepare a tokenizer for reviews on training data
 y_tokenizer = Tokenizer(num_words=tot_cnt-cnt) 
 y_tokenizer.fit_on_texts(list(y_tr))
 
 #convert text sequences into integer sequences
-y_tr_seq    =   y_tokenizer.texts_to_sequences(y_tr) 
-y_val_seq   =   y_tokenizer.texts_to_sequences(y_val) 
+y_tr_seq = y_tokenizer.texts_to_sequences(y_tr) 
+y_val_seq = y_tokenizer.texts_to_sequences(y_val) 
 
 #padding zero upto maximum length
-y_tr    =   pad_sequences(y_tr_seq, maxlen=max_summary_len, padding='post')
-y_val   =   pad_sequences(y_val_seq, maxlen=max_summary_len, padding='post')
+y_tr = pad_sequences(y_tr_seq, maxlen=max_summary_len, padding='post')
+y_val = pad_sequences(y_val_seq, maxlen=max_summary_len, padding='post')
 
 #size of vocabulary
-y_voc  =   y_tokenizer.num_words +1
+y_voc = y_tokenizer.num_words + 1
 
-# --
+#endregion
 
-#print(y_tokenizer.word_counts)
-print(y_tokenizer.word_counts['sostok'],len(y_tr))
-
-# --
+#region Clean up empty samples
 
 ind=[]
 for i in range(len(y_tr)):
@@ -206,7 +189,9 @@ for i in range(len(y_val)):
 y_val=np.delete(y_val,ind, axis=0)
 x_val=np.delete(x_val,ind, axis=0)
 
-# --
+#endregion
+
+#region Build NN
 
 K.clear_session()
 
@@ -257,17 +242,23 @@ model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
 model.summary()
 
-# --
+#endregion
+
+#region Train NN
 
 model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy')
+
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1,patience=2)
-history=model.fit([x_tr,y_tr[:,:-1]], y_tr.reshape(y_tr.shape[0],y_tr.shape[1], 1)[:,1:] ,epochs=50,callbacks=[es],batch_size=512, validation_data=([x_val,y_val[:,:-1]], y_val.reshape(y_val.shape[0],y_val.shape[1], 1)[:,1:]))
 
-# --
+model.fit([x_tr,y_tr[:,:-1]], y_tr.reshape(y_tr.shape[0],y_tr.shape[1], 1)[:,1:], epochs=50, callbacks=[es], batch_size=512, validation_data=([x_val,y_val[:,:-1]], y_val.reshape(y_val.shape[0],y_val.shape[1], 1)[:,1:]))
 
-reverse_target_word_index=y_tokenizer.index_word 
-reverse_source_word_index=x_tokenizer.index_word 
-target_word_index=y_tokenizer.word_index
+#endregion
+
+#region Decoder model
+
+reverse_target_word_index = y_tokenizer.index_word 
+reverse_source_word_index = x_tokenizer.index_word 
+target_word_index = y_tokenizer.word_index
 
 # Encode the input sequence to get the feature vector
 encoder_model = Model(inputs=encoder_inputs,outputs=[encoder_outputs, state_h, state_c])
@@ -279,7 +270,7 @@ decoder_state_input_c = Input(shape=(latent_dim,))
 decoder_hidden_state_input = Input(shape=(max_text_len,latent_dim))
 
 # Get the embeddings of the decoder sequence
-dec_emb2= dec_emb_layer(decoder_inputs) 
+dec_emb2 = dec_emb_layer(decoder_inputs) 
 # To predict the next word in the sequence, set the initial states to the states from the previous time step
 decoder_outputs2, state_h2, state_c2 = decoder_lstm(dec_emb2, initial_state=[decoder_state_input_h, decoder_state_input_c])
 
@@ -295,22 +286,24 @@ decoder_model = Model(
     [decoder_inputs] + [decoder_hidden_state_input,decoder_state_input_h, decoder_state_input_c],
     [decoder_outputs2] + [state_h2, state_c2])
 
-# --
+#endregion
+
+#region Inference
 
 def decode_sequence(input_seq):
     # Encode the input as state vectors.
     e_out, e_h, e_c = encoder_model.predict(input_seq)
-    
+
     # Generate empty target sequence of length 1.
     target_seq = np.zeros((1,1))
-    
+
     # Populate the first word of target sequence with the start word.
     target_seq[0, 0] = target_word_index['sostok']
 
     stop_condition = False
     decoded_sentence = ''
     while not stop_condition:
-      
+
         output_tokens, h, c = decoder_model.predict([target_seq] + [e_out, e_h, e_c])
 
         # Sample a token
@@ -337,22 +330,23 @@ def decode_sequence(input_seq):
 def seq2summary(input_seq):
     newString=''
     for i in input_seq:
-        if((i!=0 and i!=target_word_index['sostok']) and i!=target_word_index['eostok']):
-            newString=newString+reverse_target_word_index[i]+' '
+        if ((i != 0 and i != target_word_index['sostok']) and i != target_word_index['eostok']):
+            newString = newString + reverse_target_word_index[i]+' '
     return newString
+
 
 def seq2text(input_seq):
     newString=''
     for i in input_seq:
-        if(i!=0):
-            newString=newString+reverse_source_word_index[i]+' '
+        if (i!=0):
+            newString = newString + reverse_source_word_index[i] + ' '
     return newString
 
 
-
 for i in range(0,100):
-    print("Review:",seq2text(x_tr[i]))
-    print("Original summary:",seq2summary(y_tr[i]))
-    print("Predicted summary:",decode_sequence(x_tr[i].reshape(1,max_text_len)))
+    print("Review:", seq2text(x_tr[i]))
+    print("Original summary:", seq2summary(y_tr[i]))
+    print("Predicted summary:", decode_sequence(x_tr[i].reshape(1,max_text_len)))
     print("\n")
 
+#endregion
